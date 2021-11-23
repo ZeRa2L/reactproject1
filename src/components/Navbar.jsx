10bb4123a2827cedee7d43fe0f4e20eaa996282e
ZeRa2L <tofindaio@gmail.com>
2021-11-23 23:03:48 +0300

    slider first comit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,8 +4,9 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Container = styled.div`
-        height: 60px;
+height: 60px;
 `;
+
 const Wrapper = styled.div`
 padding: 10px 20px;
 display: flex;
