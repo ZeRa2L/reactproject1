e4ca4725f5228661976e2dc624f4292655665ea3
ZeRa2L <tofindaio@gmail.com>
2022-01-08 22:23:19 +0300

    login and start a cart

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,23 @@
+import Navbar from '../components/Navbar';
+import Announcement from '../components/Announcement';
+import Footer from '../components/Footer';
+import styled from "styled-components"
+
+const Container = styled.div`
+ 
+`;
+
+const Cart = () => {
+    return (
+        <Container>
+            <Navbar/>
+            <Announcement/>
+            <Wrapper>
+                
+            </Wrapper>
+            <Footer/>
+        </Container>
+    )
+}
+
+export default Cart
