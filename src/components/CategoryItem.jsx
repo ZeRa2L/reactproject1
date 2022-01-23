34e9858d43e3a23021d48752a942939f43bf2a00
ZeRa2L <tofindaio@gmail.com>
2022-01-24 01:05:08 +0300

    first part ended

--- src/components/CategoryItem.jsx
+++ src/components/CategoryItem.jsx
@@ -1,4 +1,5 @@
 import styled from "styled-components"
+import { mobile } from "../responsive";
 
 const Container = styled.div`
 flex: 1;
@@ -11,6 +12,7 @@ width: 100%;
 height: 100%;
 background-color: green;
 object-fit: cover;
+${mobile({ height: "30vh" })}
 `;
 const Info = styled.div`
 position: absolute;
